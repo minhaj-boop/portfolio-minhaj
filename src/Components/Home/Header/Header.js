cdd64629025cf57d6a6beb92eccb1592d462495d
minhaj-boop <imminhajj@gmail.com>
2022-02-17 02:55:25 +0600

    ..

--- src/Components/Home/Header/Header.js
+++ src/Components/Home/Header/Header.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import './Header.css';
-import logo from '../../../pic/logo.png';
 
 const Header = () => {
     //when scroll header at top
@@ -17,7 +16,7 @@ const Header = () => {
             <header className='header'>
                 <div className="container d_flex">
                     <div className="logo">
-                        <img src={logo} alt="" />
+                        <img src="" alt="" />
                     </div>
 
                     <div className="navlink">
